c1b1be84229d34089a9c2ebab18cc27c26bc01a0
lucasGonzalez591 <lucasunited98@gmail.com>
2026-02-11 12:16:39 -0300

    primer commit

---
+++ src/test/java/ar/com/practicaproyecto/PracticaProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.practicaproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
